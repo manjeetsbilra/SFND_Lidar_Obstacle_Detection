6e053821d6dc978e8164aad15da8ab2a545b9b9a
Manjeet Singh  Bilra <manjeetsbilra@gmail.com>
2021-01-09 19:52:32 +0100

    Lidar layers updated to 32 instead of 8

--- src/sensors/lidar.h
+++ src/sensors/lidar.h
@@ -86,16 +86,16 @@ struct Lidar
 		: cloud(new pcl::PointCloud<pcl::PointXYZ>()), position(0,0,2.6)
 	{
 		// TODO:: set minDistance to 5 to remove points from roof of ego car
-		minDistance = 5;// Distance of rays from roof changed from 0 to 5m
+		minDistance = 4;// Distance of rays from roof changed from 0 to 5m
 		maxDistance = 50;
 		resoultion = 0.2;
 		// TODO:: set sderr to 0.2 to get more interesting pcd files
-		sderr = 0.2;// Set the noise of sensor from 0m to 0.2m 
+		sderr = 0.2;// Set the noise of sensor from 0m to 0.2m
 		cars = setCars;
 		groundSlope = setGroundSlope;
 
 		// TODO:: increase number of layers to 8 to get higher resoultion pcd
-		int numLayers = 8;// Layers changed from 3 to 8
+		int numLayers = 32;// Layers changed from 3 to 8
 		// the steepest vertical angle
 		double steepestAngle =  30.0*(-pi/180);
 		double angleRange = 26.0*(pi/180);
